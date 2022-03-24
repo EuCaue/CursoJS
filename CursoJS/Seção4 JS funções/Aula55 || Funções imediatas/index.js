40e4216d0a80ebf4d3b35116a669d687328ccce8
EuCaue <caueadriano.castro@gmail.com>
2022-03-24 16:40:06 -0300

    Aula55 || Funções imediatas

---
+++ CursoJS/Seção4 JS funções/Aula55 || Funções imediatas/index.js
@@ -0,0 +1,6 @@
+// IIFE => Immediately  invoked function expression
+// Função é chamda assim ue o código inicia.
+(function () {
+    const nome = 'comi o cu de quem tá lendo'
+    console.log(nome);
+})();
