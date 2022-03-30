9e58cf7f88090c0f0b651c1462d97e88e0d670c3
EuCaue <caueadriano.castro@gmail.com>
2022-03-30 17:15:09 -0300

    Aula64 || Concatenando Arrays

---
+++ CursoJS/Seção5 JS Arrays/Aula64 || Concatenando Arrays/index.js
@@ -0,0 +1,19 @@
+// const arr1 = [1,2,3];
+// const arr2 = [4,5,6];
+// const arr4 = [7,8,9];
+
+// Serve para concatenar arrays.
+// const arr3 = arr1.concat(arr2,arr4);
+ 
+// console.log(arr3);
+
+// const arr1 = [1,2,3];
+// const arr2 = [4,5,6];
+// const arr4 = [7,8,9];
+
+// // Juntando arrays com Spreed Operator
+// const arr3 = [...arr1,...arr2, ...arr4];
+// console.log(arr3);
+
+
+
