4718fd379f98b4f633a626024d96f89b3c3adc8e
EuCaue <caueadriano.castro@gmail.com>
2022-04-06 22:33:41 -0300

    Aula88 || ForEach

---
+++ CursoJS/Seção5 JS Arrays/Aula69 || ForEach/index.js
@@ -0,0 +1,9 @@
+const a1 = [10,20,30,40,50,60,70,80,90]
+
+
+// forEach, recebe uma função, que pode receber o valor, indice e array.
+// Faz iteração sobre o array, mesma coisa do forOf, forIn ou for.
+
+a1.forEach( (valor, indice, array) => {
+    
+});
