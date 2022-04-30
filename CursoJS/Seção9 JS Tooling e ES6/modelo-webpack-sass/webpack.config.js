33e824b1bbd410c0b5db9638f70d31e35e816bdf
EuCaue <caueadriano.castro@gmail.com>
2022-04-29 21:42:56 -0300

    modelo-sass

---
+++ CursoJS/Seção9 JS Tooling e ES6/modelo-webpack-sass/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path'); // CommonJS !==  diferente do ES6
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = { 
+  plugins: [new MiniCssExtractPlugin()],
+  // Só gera o arquivo com um monte de 
+  // Pórem, mais rápido
+  mode: 'development',
+  
+  // Arquivo de entrada, da onde o Source está vindo
+  entry: './src/main.js',
+  
+  // "Resolvendo" o caminho para jogar o output do bundle.js
+  // Multi-sistema, ___dirname significa que ele está buscando o caminho relativo
+  // para o diretorio desse arquivo, e procurando as pastas que foi passada
+  // como parametros
+  // filename é nome do arquivo do -o
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets', 'dist'),
+    filename: 'bundle.js',
+  },
+
+  // Regras para os modulos
+  //* Exclude está fazendo o webpack não analisar a pasta do node_modules por motivos obvios
+  //* Test, é para verificar se o arquivo termina em JS  
+  //* Loader, é para escolhe quando você o loader para transpilar o código 
+  //* Options, são as opções do loader
+  //* preset tá escolhendo quando Preset do babel no caso usar
+
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: { 
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
+        }
+      }
+    }, { 
+      test: /\.s[ac]ss$/i,
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+    }],
+  },
+
+  // Serve para mapear os arquivos que geram os bundles
+  // para quando de bug, você receber em qual arquivo gerou o erro no bundle
+  devtool: 'source-map',
+}
