a4cc52d4a1f7e8ac8291a2e67956863b4cb92672
EuCaue <caueadriano.castro@gmail.com>
2022-05-01 22:04:16 -0300

    Aula97 || CoreJS e Regenerator

---
+++ CursoJS/Seção9 JS Tooling e ES6/Aula97 || CoreJS e Regenerator/src/main.js
@@ -0,0 +1,8 @@
+// Para funcionar em navegadores mais antigo
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import executa from './modules/promises';
+
+import './assets/css/style.css';
+executa();
