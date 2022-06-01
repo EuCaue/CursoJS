f110c522bb9eec090bd00db0cebed0223d3dcfbb
EuCaue <caueadriano.castro@gmail.com>
2022-06-01 19:27:39 -0300

    CRUD de alunos

--- CursoJS/secao12-api-rest/src/models/Aluno.js
+++ CursoJS/secao12-api-rest/src/models/Aluno.js
@@ -4,12 +4,65 @@ import Sequelize, { Model } from 'sequelize';
 export default class Aluno extends Model {
   static init(sequelize) {
     super.init({
-      nome: Sequelize.STRING,
-      sobrenome: Sequelize.STRING,
-      email: Sequelize.STRING,
-      idade: Sequelize.INTEGER,
-      peso: Sequelize.FLOAT,
-      altura: Sequelize.FLOAT,
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Name must be between 3 and 255 characters ',
+          },
+        },
+      },
+      sobrenome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Sobrenome must be between 3 and 255 characters ',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email already exists',
+        },
+        validate: {
+          isEmail: {
+            msg: 'Email must be valid',
+          },
+        },
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'Idade must be integer',
+          },
+        },
+      },
+      peso: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Peso must be integer or float number',
+          },
+        },
+      },
+      altura: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Altura must be integer or float number',
+          },
+        },
+      },
     }, {
       sequelize,
     });
