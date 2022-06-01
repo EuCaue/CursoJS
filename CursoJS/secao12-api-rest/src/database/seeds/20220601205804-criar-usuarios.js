0ccc42b641813f40d1bf95cb3bcbc5a6e1c8867a
EuCaue <caueadriano.castro@gmail.com>
2022-06-01 18:16:37 -0300

    seeds

--- CursoJS/secao12-api-rest/src/database/seeds/20220601205804-criar-usuarios.js
+++ CursoJS/secao12-api-rest/src/database/seeds/20220601205804-criar-usuarios.js
@@ -1,9 +1,31 @@
+const bcryptjs = require('bcryptjs');
+
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.bulkInsert('People', [{
-      name: 'John Doe',
-      isBetaMember: false,
-    }], {});
+    await queryInterface.bulkInsert('users', [{
+      nome: 'Vraulisson',
+      email: 'vraulisson@gmail.com',
+      password_hash: await bcryptjs.hash('123456', 8),
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      nome: 'Vraulisson 2',
+      email: 'vraulisson2@gmail.com',
+      password_hash: await bcryptjs.hash('123456', 8),
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      nome: 'Vraulisson 3',
+      email: 'vraulisson3@gmail.com',
+      password_hash: await bcryptjs.hash('123456', 8),
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    ], {
+
+    });
   },
 
   // eslint-disable-next-line no-empty-function
