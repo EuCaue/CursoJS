c7fda8ee730fb6ea8456af3579d23afc1f02064b
EuCaue <caueadriano.castro@gmail.com>
2022-06-02 19:04:47 -0300

    Tratando arquivos enviados

--- CursoJS/secao12-api-rest/src/config/multer.js
+++ CursoJS/secao12-api-rest/src/config/multer.js
@@ -5,6 +5,15 @@ import { extname, resolve } from 'path';
 const random = () => Math.floor(Math.random() * 10000 + 10000);
 // Exportando por padrão a config do multer
 export default {
+  // Filtrando o tipo do arquivo, para passar somente JPG && PNG
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      // Retornando error caso não seja JPG ou PNG
+      return cb(new multer.MulterError('Filetype must be PNG or JPG'));
+    }
+
+    return cb(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       // Resolvendo o caminho para o diretorio de uploads
--- CursoJS/secao12-api-rest/src/controllers/PhotosController.js
+++ CursoJS/secao12-api-rest/src/controllers/PhotosController.js
@@ -1,7 +1,24 @@
+import multer from 'multer';
+import multerConfig from '../config/multer';
+
+const upload = multer(multerConfig).single('photo');
+
 // Controller de photos
 class PhotosController {
   async store(req, res) {
-    res.json(req.file);
+    // Retornando erro se tiver erro no upload
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [
+            err.code,
+          ],
+        });
+      }
+
+      // retornando os dados do arquivo enviado
+     return res.json(req.file);
+    });
   }
 }
 
