9559d4c6312058daaf99d2e20a387e99f54fdffe
EuCaue <caueadriano.castro@gmail.com>
2022-06-20 19:59:28 -0300

    useNavigate

--- CursoJS/secao15-react-redux-etc/react-base/src/pages/Page404/index.js
+++ CursoJS/secao15-react-redux-etc/react-base/src/pages/Page404/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+// import history from '../../services/history';
 import { Container } from '../../styles/GlobalStyles';
 
 export default function Page404() {
+  const Navigate = useNavigate();
+  Navigate('/');
   return (
     <Container>
       <h1>404 - Page not found</h1>
