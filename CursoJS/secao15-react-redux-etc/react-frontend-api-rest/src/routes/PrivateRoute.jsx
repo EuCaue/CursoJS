4a6a8cdfcd18416ba9dd942c76f27e0a1fa4a4de
EuCaue <caueadriano.castro@gmail.com>
2022-07-04 20:38:19 -0300

    Comments in PrivateRoute

--- CursoJS/secao15-react-redux-etc/react-frontend-api-rest/src/routes/PrivateRoute.jsx
+++ CursoJS/secao15-react-redux-etc/react-frontend-api-rest/src/routes/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Navigate, Outlet } from 'react-router-dom';
 
-// If is loggendIn = true, Navigate to the child page in Route, if false, Return to homepage
+// If is Open = true, Navigate to the child page in Route, if false, Return to homepage
 export default function PrivateRoute({ Open }) {
   return Open ? <Outlet /> : <Navigate to="/" />;
 }
