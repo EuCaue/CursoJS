fd3629641672a92119efd1bb27f98eb8759bd8ed
EuCaue <caueadriano.castro@gmail.com>
2022-07-04 18:29:26 -0300

    update cors

--- CursoJS/secao12-api-rest/src/app.js
+++ CursoJS/secao12-api-rest/src/app.js
@@ -19,17 +19,18 @@ import photosRoutes from './routes/photosRoutes';
 const whitelist = [
   'http://34.176.148.27',
   'http://localhost:3000',
+  'http://localhost:3001',
 ];
 
-const corsOptions = {
-  origin(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
+//const corsOptions = {
+  //origin(origin, callback) {
+    //if (whitelist.indexOf(origin) !== -1 || !origin) {
+      //callback(null, true);
+    //} else {
+     // callback(new Error('Not allowed by CORS'));
+   // }
+ // },
+//};
 
 // Classe do app
 class App {
@@ -41,7 +42,7 @@ class App {
   }
 
   middlewares() {
-    this.app.use(cors(corsOptions));
+    this.app.use(cors());
     this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
